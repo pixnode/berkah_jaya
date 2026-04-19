dd4a328ddd746369b6bd738a8db2f696cf04cec2
Pixnode <dev@pixnode.com>
2026-04-20 01:38:14 +0700

    Add PM2 ecosystem configuration

---
+++ ecosystem.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  apps : [{
+    name: "btc-sniper",
+    script: "./btc_sniper/main.py",
+    interpreter: "python3",
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: "1G",
+    env: {
+      NODE_ENV: "production",
+      PYTHONUNBUFFERED: "1",
+      PYTHONPATH: "./btc_sniper",
+      LOG_LEVEL: "INFO"
+    },
+    error_file: "./output/pm2-error.log",
+    out_file: "./output/pm2-out.log",
+    log_date_format: "YYYY-MM-DD HH:mm:ss",
+    merge_logs: true
+  }]
+}
